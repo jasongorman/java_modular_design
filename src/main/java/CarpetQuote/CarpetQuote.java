af4301521fd9e4200200101a09fd88e99a628f43
Jason Gorman <jason.gorman@codemanship.com>
2020-06-22 08:46:57 +0100

    Initial commit

---
+++ src/main/java/CarpetQuote/CarpetQuote.java
@@ -0,0 +1,12 @@
+package CarpetQuote;
+
+public class CarpetQuote {
+    public double calculate(double length, double width, double pricePerSqM, boolean roundUpToSqM) {
+        double area = length * width;
+
+        if(roundUpToSqM)
+            area = Math.ceil(area);
+
+        return area * pricePerSqM;
+    }
+}
---
+++ src/test/java/CarpetQuoteTest.java
@@ -0,0 +1,21 @@
+import CarpetQuote.CarpetQuote;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CarpetQuoteTest {
+
+    @Test
+    public void carpetPriceNotRounded(){
+        CarpetQuote quote = new CarpetQuote();
+        assertEquals(1562.5,
+                quote.calculate(12.5, 12.5, 10.0, false), 0.0);
+    }
+
+    @Test
+    public void carpetPriceRounded(){
+        CarpetQuote quote = new CarpetQuote();
+        assertEquals(1570.0,
+                quote.calculate(12.5, 12.5, 10.0, true), 0.0);
+    }
+}
